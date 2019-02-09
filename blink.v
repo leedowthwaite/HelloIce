42f8f9de53434e7c7d8f096d48acb4ad85892c28
Lee Dowthwaite <leedowthwaite@gmail.com>
2019-02-09 19:27:36 +0000

    Added comments etc

--- blink.v
+++ blink.v
@@ -1,6 +1,9 @@
+`default_nettype none
 
+// define a Blink module
 module Blink(input CLK_IN, output GLED5, output RLED1, output RLED2, output RLED3, output RLED4);
 
+	// define a 24-bit counter to divide the clock down from 12MHz
 	localparam WIDTH = 24;
 	reg [WIDTH-1:0] counter;
 
